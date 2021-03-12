329c8a446a2d8dce37e2ecf04a74584f3285350e
abahkau <80201622+abahkau@users.noreply.github.com>
2021-03-12 20:12:53 +0800

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,7 +1,8 @@
 export MAX_DOWNLOAD_SPEED=0
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
 export MAX_CONCURRENT_DOWNLOADS=10
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false \
   --max-connection-per-server=16 --rpc-max-request-size=1024M \
-  --seed-time=0.01 --bt-stop-timeout=300 --min-split-size=10M --follow-torrent=mem --split=10 \
+  --bt-tracker="[$tracker_list]" --seed-time=0.01 --bt-stop-timeout=300 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
